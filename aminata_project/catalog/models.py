aca46fd61b9c868d8a3f498822221db30ddeddb2
Linah Wainaina <wainainalinahw@gmail.com>
2023-09-09 16:53:12 +0300

    changes updated

--- aminata_project/catalog/models.py
+++ aminata_project/catalog/models.py
@@ -6,7 +6,6 @@ class Category(models.Model):
 
 class Catalog(models.Model):
     name = models.CharField(max_length=32)
-    description = models.TextField()  # Use TextField for longer descriptions
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # ForeignKey to Category model
-    image = models.ImageField(upload_to='catalog_images/')  # Example directory for storing images
-
+    description = models.TextField()  
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  
+    image = models.ImageField(upload_to='catalog_images/')  
