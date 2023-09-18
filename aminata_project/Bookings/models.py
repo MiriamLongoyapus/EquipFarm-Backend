43f8dbb72233dc22354891229dd8c4dae77b634b
chirchirmercy <124134209+chirchirmercy@users.noreply.github.com>
2023-09-18 19:10:34 +0300

    Update models.py

--- aminata_project/Bookings/models.py
+++ aminata_project/Bookings/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # import random
 
 class Bookings(models.Model):
-#     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 #     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    customer_name = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     equipment_name = models.CharField(max_length=32)
     equipment_category = models.CharField(max_length=32)
     duration = models.DurationField()
