871fba43facb58a62880e72084d3985fc35b52f3
chirchirmercy <cmercycheptoo@gmail.com>
2023-09-24 21:42:37 +0300

    commited changes

--- aminata_project/Bookings/models.py
+++ aminata_project/Bookings/models.py
@@ -2,7 +2,6 @@ from django.db import models
 # from users.models import CustomUser 
 
 class Bookings(models.Model):
-
     #customer_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     #equipment_name = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=32)
