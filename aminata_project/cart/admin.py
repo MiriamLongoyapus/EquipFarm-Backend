ddc786f53bc83a33c47d9b8ab1ee5675aba783e3
Student <leilahashurgmail.com>
2023-09-25 22:07:46 +0300

    reduced files

--- aminata_project/cart/admin.py
+++ aminata_project/cart/admin.py
@@ -1,11 +1,10 @@
 
-
 from django.contrib import admin
 from .models import Cart
 
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('user', 'created_at', 'product_name', 'quantity', 'price', 'total_price')
-    list_filter = ('user', 'created_at')
-    search_fields = ('product_name', 'user__username')
+    list_display = ( 'created_at', 'quantity', 'price', 'total_price')
+    
+    search_fields = ('equipment_name', 'user__username')  # Change 'product_name' to 'equipment_name__name'
 
 admin.site.register(Cart, CartAdmin)
--- aminata_project/cart/models.py
+++ aminata_project/cart/models.py
@@ -6,11 +6,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    product_name = models.CharField(max_length=100,unique=True)
+    # equipment_name = models.ForeignKey(equipment_name, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=None)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    
     
 
     def total_price(self):
