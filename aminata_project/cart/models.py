9bc32ca6881325ad6f2595b980e723d56d3fee35
Student <leilahashurgmail.com>
2023-09-25 22:27:41 +0300

    reduced files

--- aminata_project/cart/models.py
+++ aminata_project/cart/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Cart(models.Model):
     # user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    # equipment_name = models.ForeignKey(catalog, on_delete=models.CASCADE)
+    # equipment_name = models.ForeignKey(Catalog, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=None)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
