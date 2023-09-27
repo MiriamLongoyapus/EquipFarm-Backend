a41ed0af456ba741ab77dc6e7a7c79580d8ae13a
Linah Wainaina <wainainalinahw@gmail.com>
2023-09-27 19:46:54 +0300

    added a category page

--- aminata_project/category/models.py
+++ aminata_project/category/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
+
 class Category(models.Model):
-      title=models.CharField(max_length=100)
-      detail=models.TextField()
-      image = models.ImageField(upload_to='catalogue_images/')
-      class Meta:
-          verbose_name_plural='2. Categories'
-      def __str__(self):
-              return self.title
+    DAIRY_CHOICES = [
+        ("milking_machine", "Milking-machine"),
+        ("milk_pasteurizer", "Milk_pasteurizer"),
+        ("cow_brushes", "Cow_brushes"),
+        ("testing_equipment", "Testing_equipment"),
+    ]
+    FARM_CHOICES = [
+        ("plow", "Plow"),
+        ("harrow", "Harrow"),
+        ("planter", "Planter"),
+    ]
+    POUTRY_CHOICES = [
+        ("nesting_boxes", "Nesting_boxes"),
+        ("egg_incubators", "Egg_incubators"),
+        ("automatic_waterers", "Automatic_waterers"),
+    ]
+  
+    farm = models.CharField(choices=FARM_CHOICES, max_length=20, default='plow')
+    poultry = models.CharField(choices=POUTRY_CHOICES, max_length=20,default='nesting_boxes')
+    dairy =  models.CharField(choices=DAIRY_CHOICES, max_length=20, default='milking_machine')
 
 
+    def __str__(self):  
+     return self.get_dairy_display() if self.dairy else (self.get_farm_display() if self.farm else self.get_poultry_display())
+ 
