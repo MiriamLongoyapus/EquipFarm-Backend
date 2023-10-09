9141beae0ce26d84fb513e1c15185f60cab1ed15
Linah Wainaina <wainainalinahw@gmail.com>
2023-10-09 11:39:09 +0300

    made changes from the app

--- aminata_project/settings.py
+++ aminata_project/settings.py
@@ -91,16 +91,7 @@ from decouple import config
 
 import os
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT'),
-#     }
-# }
+
 if config("MODE") == "dev":
     DATABASES = {
         "default": {
